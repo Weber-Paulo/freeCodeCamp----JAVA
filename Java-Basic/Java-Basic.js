22c2f082c2a7236d315d545dbccba34fafdc2879
Weber-Paulo <paulomorandini@gmail.com>
2022-11-10 20:55:18 -0300

    Basic

---
+++ Java-Basic/Java-Basic.js
@@ -0,0 +1,21 @@
+// This is an in-line comment.
+/* This is a
+multi-line comment */
+
+/* cria uma variável chamada ourName. Em JavaScript, terminamos uma instrução com ponto e vírgula. Nomes de variáveis podem ser formados por números, letras e $ ou _, mas não podem conter espaços ou começar com um número. */
+var myName;
+var ourName;
+
+/* Atribua o valor 7 para a variável a.*/
+var a;
+a = 7;
+
+/* Atribua o conteúdo de a para a variável b.*/
+var a;
+a = 7;
+var b;
+b = a;
+
+/* Defina uma variável a com var e a inicialize com o valor de 9.*/
+var a = 9;
+
