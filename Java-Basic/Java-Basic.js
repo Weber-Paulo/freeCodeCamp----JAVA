70d16621a97846b4fb39c67a6eb1bcbf2bcbfc5f
Weber-Paulo <paulomorandini@gmail.com>
2022-11-10 21:24:28 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -27,9 +27,9 @@ console.log(myFirstName)
 console.log(myLastName)
 
 /* Inicialize as três variáveis a, b e c com 5, 10, e "I am a" respectivamente para que eles não sejam undefined.*/
-var a = "5";
-var b = "10";
+var a = 5;
+var b = 10;
 var c = "I am a";
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+console.log(a = a + 1);
+console.log(b = b + 5);
+console.log(c = c + " String!");
