f2ae0c6a089ce6f78120f194ebb3d442f2fc9f66
Weber-Paulo <paulomorandini@gmail.com>
2022-11-10 21:18:54 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -18,4 +18,18 @@ b = a;
 
 /* Defina uma variável a com var e a inicialize com o valor de 9.*/
 var a = 9;
+console.log(a)
 
+/* Crie duas novas variáveis de string: myFirstName e myLastName e atribua a elas os valores do seu primeiro e último nome, respectivamente. */
+var myFirstName = "Paulo";
+var myLastName = "Weber";
+console.log(myFirstName)
+console.log(myLastName)
+
+/* Inicialize as três variáveis a, b e c com 5, 10, e "I am a" respectivamente para que eles não sejam undefined.*/
+var a = "5";
+var b = "10";
+var c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
