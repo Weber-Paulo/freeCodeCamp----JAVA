470c07c5063e4e68ed5e42ac906381dd6cc8f4ad
Weber-Paulo <paulomorandini@gmail.com>
2022-11-17 20:27:34 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -432,3 +432,24 @@ const lastName3 = "Lovelace";
 // Altere apenas o código abaixo desta linha
 const lastLetterOfLastName = lastName3[lastName.length - 1]; // Altere esta linha
 console.log(lastLetterOfLastName)
+
+/*Usar notação de colchetes para descobrir o enésimo caractere antes do último em uma string
+Você pode usar o mesmo princípio que nós acabamos de usar para recuperar o último caractere em uma string, para recuperar o enésimo caractere antes do último caractere.
+
+Por exemplo, você pode pegar o valor da antepenúltima letra da string const firstName = "Augusta" usando firstName[firstName.length - 3]
+
+Exemplo:
+
+const firstName = "Augusta";
+const thirdToLastLetter = firstName[firstName.length - 3];
+thirdToLastLetter teria o valor da string s.
+
+Use notação de colchetes para descobrir o penúltimo caractere na string lastName.
+
+Dica: tente olhar o exemplo acima se você ficar travado.*/
+// Configuração
+const lastName4 = "Lovelace";
+
+// Altere apenas o código abaixo desta linha
+const secondToLastLetterOfLastName = lastName4[lastName.length -2]; // Altere esta linha
+console.log(secondToLastLetterOfLastName)
