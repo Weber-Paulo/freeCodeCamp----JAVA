e47f0029c25a32e64d838e562c45951cd2ba7da5
Weber-Paulo <paulomorandini@gmail.com>
2022-11-17 20:56:39 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -474,3 +474,13 @@ const myAdverb = "quickly";
 const wordBlanks = "My "+myNoun+", and we "+myVerb+" comer "+myAdjective+" muito "+myAdverb+"."; // Altere esta linha
 // Altere apenas o código acima desta linha
 console.log(wordBlanks)
+
+/*Armazenar múltiplos valores em uma variável usando arrays JavaScript
+Com as variáveis de array em JavaScript, podemos armazenar diversos dados em um único lugar.
+
+Você começa uma declaração de um array com a abertura de um colchetes, terminando com o fechamento do colchetes e colocando vírgulas entre cada entrada, dessa forma:
+
+const sandwich = ["peanut butter", "jelly", "bread"];
+Modifique o novo array myArray para que contenha uma string e um número (nessa ordem).*/
+const myArray = ["big bang", 123];
+console.log(myArray)
