da9d2edcf78c634a5a1a7d46a951c3440edecace
Weber-Paulo <paulomorandini@gmail.com>
2022-11-21 21:34:20 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -495,3 +495,40 @@ Crie um array aninhado chamado de myArray.*/
 // Altere apenas o código abaixo desta linha
 const myArray2 = [["Bulls, 23"], ["white Sox", 45]];
 console.log(myArray2)
+
+/*Acessar array de dados com índices
+Podemos acessar os dados dentro de arrays usando indexes.
+
+Os índices de um array são escritos na mesma notação com colchetes que as strings usam. Porém, em vez de especificar um caractere, eles estão especificando um item do array. Assim como ocorre com as strings, os arrays usam indexação de base zero, de forma que o primeiro elemento de um array possui índice 0.
+
+
+Exemplo
+
+const array = [50, 60, 70];
+console.log(array[0]);
+const data = array[1];
+O console.log(array[0]) exibirá 50 e data terá o valor de 60.
+
+Crie uma variável chamada myData e defina-a como igual ao primeiro valor de myArray usando notação de colchetes.*/
+const myArray3 = [50, 60, 70];
+const myData = myArray3[0]
+console.log(myData)
+
+/*Modificar dados de array com índices
+Ao contrário das strings, as entradas de arrays são mutáveis e podem ser alteradas livremente, mesmo se o array foi declarado com const.
+
+Exemplo
+
+const ourArray = [50, 40, 30];
+ourArray[0] = 15;
+ourArray agora tem o valor [15, 40, 30].
+
+Observação: não deve haver espaços entre o nome do array e os colchetes, como array [0]. Embora JavaScript seja capaz de processar isso corretamente, isso pode confundir outros programadores lendo seu código.
+
+Modifique o dado armazenado no índice 0 de myArray para um valor de 45.*/
+// Configuração
+const myArray4 = [18, 64, 99];
+
+// Altere apenas o código abaixo desta linha
+myArray4[0] = 45;
+console.log(myArray4)
