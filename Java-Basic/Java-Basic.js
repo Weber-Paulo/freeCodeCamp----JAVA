2b46c4ff87a67a2f5176843219432c1dc1f25831
Weber-Paulo <paulomorandini@gmail.com>
2022-11-23 22:08:57 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -657,3 +657,21 @@ O primeiro elemento em cada sub-array deve conter uma string com o nome do item.
 Deve haver pelo menos 5 sub-arrays na lista.*/
 const myList = [["Chocolate Bar", 15], ["Bread", 30], ["Juice", 10], ["Grape", 5], ["Computer", 2]];
 console.log(myList)
+
+/*Escrever JavaScript reutilizável com funções
+Em JavaScript, nós podemos dividir nosso código em partes reutilizáveis chamadas de funções.
+
+Aqui está um exemplo de uma função:
+
+function functionName() {
+  console.log("Hello World");
+}
+Você pode chamar ou invocar essa função ao usar seu nome seguido de parênteses, da seguinte forma: functionName(); Cada vez que a função é chamada, imprimirá no console a mensagem Hello World. Todo o código entre as chaves será executado toda vez que uma função for chamada.
+
+Crie uma função chamada reusableFunction que imprime a string Hi World no console.
+Chame a função.*/
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction()
