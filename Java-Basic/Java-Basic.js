77ba93105597205399f5fc7a5a51f7860fcceff5
Weber-Paulo <paulomorandini@gmail.com>
2022-11-24 21:26:22 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -693,3 +693,54 @@ function functionWithArgs(Args1, Args2) {
 }
 functionWithArgs(1,2);
 functionWithArgs(7,9)
+
+/*Retornar um valor de uma função com return
+Nós podemos passar valores para uma função com argumentos. Você pode usar uma instrução return para enviar um valor para fora de uma função.
+
+Exemplo
+
+function plusThree(num) {
+  return num + 3;
+}
+
+const answer = plusThree(5);
+answer tem o valor de 8.
+
+plusThree recebe um argumento para num e retorna um valor igual a num + 3.
+
+Crie uma função timesFive que aceita um argumento, multiplica ele por 5 e retorna o novo valor.*/
+function timesFive(num) {
+  return num * 5;
+}
+timesFive(5);
+timesFive(2);
+timesFive(0)
+
+/*Conhecer o escopo global e funções
+Em JavaScript, escopo refere-se à visibilidade de variáveis. Variáveis que são definidas fora de um bloco de função tem o escopo Global. Isso significa que elas podem ser vistas em qualquer lugar no seu código JavaScript.
+
+Variáveis que são declaradas sem a palavra-chave let ou const são automaticamente criadas no escopo global. Isso pode criar consequências indesejadas em outro lugar no seu código ou quando executar uma função novamente. Você sempre deve declarar suas variáveis com let ou const.
+
+Usando let ou const, declare uma variável global chamada myGlobal fora de qualquer função. Inicialize-a com o valor de 10.
+
+Dentro da função fun1, atribua 5 para oopsGlobal sem usar as palavras-chave var, let ou const.*/
+// Declare a variável myGlobal abaixo desta linha
+let myGlobal=10
+
+function fun1() {
+  // Atribua 5 para oopsGlobal aqui
+oopsGlobal=5;
+}
+
+// Altere apenas o código acima desta linha
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
