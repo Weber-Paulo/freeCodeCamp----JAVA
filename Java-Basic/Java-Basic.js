cd53c65fb186c4152937a62e1ce3f62b8782bd03
Weber-Paulo <paulomorandini@gmail.com>
2022-11-27 20:39:17 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -744,3 +744,58 @@ function fun2() {
   }
   console.log(output);
 }
+
+/*Conhecer o escopo local e funções
+Variáveis que são declaradas dentro de uma função, assim como parâmetros das funções, possuem escopo local. Isso significa que elas são visíveis apenas dentro da função.
+
+Aqui está uma função myTest com uma variável local chamada loc.
+
+function myTest() {
+  const loc = "foo";
+  console.log(loc);
+}
+
+myTest();
+console.log(loc);
+A chamada da função myTest() vai exibir a string foo no console. A linha console.log(loc) (fora da função myTest) vai lançar um erro, já que loc não foi definido fora da função.
+
+O editor possui dois console.log para ajudar você a ver o que está acontecendo. Verifique o console enquanto codifica para ver como muda. Declare uma variável local myVar dentro de myLocalScope e rode os testes.
+
+Observação: o console ainda exibirá ReferenceError: myVar is not defined, mas isso não causará falha nos testes.*/
+
+function myLocalScope() {
+  // Altere apenas o código abaixo desta linha
+const myVar = "foot"
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Executar e verificar o console
+// myVar não está definida fora do myLocalScope
+console.log('outside myLocalScope', myVar);
+
+/*Diferenciar escopo global e local em funções
+É possível ter as variáveis local e global com o mesmo nome. Quando você faz isso, a variável local tem precedência sobre a variável global.
+
+Neste exemplo:
+
+const someVar = "Hat";
+
+function myFun() {
+  const someVar = "Head";
+  return someVar;
+}
+A função myFun retornará a string Head porque a versão local da variável está presente.
+
+Adicione uma variável local para a função myOutfit para sobrescrever o valor de outerWear com a string sweater.*/
+// Configuração
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Altere apenas o código abaixo desta linha
+const outerWear = "sweater"
+  // Altere apenas o código acima desta linha
+  return outerWear;
+}
+
+myOutfit();
