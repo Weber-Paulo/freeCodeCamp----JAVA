2f0f71527bedb6f19b59d59cbd92304a9f0385b5
Weber-Paulo <paulomorandini@gmail.com>
2022-11-28 20:19:41 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -819,15 +819,35 @@ Crie uma função addFive sem qualquer argumento. Essa função adiciona 5 à va
 let sum1 = 0;
 
 function addThree() {
-  sum = sum + 3;
+  sum1 = sum1 + 3;
 }
 
 // Altere apenas o código abaixo desta linha
 function addFive() {
-  sum = sum + 5;
+  sum1 = sum1 + 5;
 }
 
 // Altere apenas o código acima desta linha
 
 addThree();
 addFive();
+
+/*Atribuir com o valor retornado
+Se você se lembrar de nossa discussão sobre como armazenar valores com o operador de atribuição, tudo à direita do sinal de igual é resolvido antes de o valor ser atribuído. Isso significa que podemos pegar o valor de retorno de uma função e atribuí-lo a uma variável.
+
+Assuma que temos uma função predefinida chamada sum, que soma dois números. Então:
+
+ourSum = sum(5, 12);
+chamará a função sum, a qual retorna o valor 17 e então atribui este valor à variável ourSum.
+
+Chame a função processArg com um argumento de 7 e atribui o retorno do seu valor para a variável processed.*/
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+console.log(processed)
