6d7d4292af0ce5a96dba5f5720993b30ea775f1c
Weber-Paulo <paulomorandini@gmail.com>
2022-12-04 21:19:32 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -1281,3 +1281,52 @@ function testElseIf(val) {
 }
 }
 testElseIf(7);
+
+/*Ordem Lógica em Instruções If Else
+A ordem é importante em if, else ifdeclarações.
+
+A função é executada de cima para baixo, portanto, você deve ter cuidado com a instrução que vem primeiro.
+
+Tome essas duas funções como exemplo.
+
+Aqui está o primeiro:
+
+function foo(x) {
+  if (x < 1) {
+    return "Less than one";
+  } else if (x < 2) {
+    return "Less than two";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+E o segundo apenas muda a ordem das declarações:
+
+function bar(x) {
+  if (x < 2) {
+    return "Less than two";
+  } else if (x < 1) {
+    return "Less than one";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+Embora essas duas funções pareçam quase idênticas, se passarmos um número para ambas, obteremos saídas diferentes.
+
+foo(0)
+bar(0)
+foo(0)retornará a string Less than onee bar(0)retornará a string Less than two.
+
+Altere a ordem da lógica na função para que ela retorne as instruções corretas em todos os casos.*/
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
