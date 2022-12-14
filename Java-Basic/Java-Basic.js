7145e014e3f75a143b7a1c5b2063ea46d3255fdf
Weber-Paulo <paulomorandini@gmail.com>
2022-12-13 21:18:30 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -1757,3 +1757,64 @@ const myDog = {
 
   // Altere apenas o código acima desta linha
 };
+
+/*Acessar propriedades de objetos com notação de pontos
+Existem duas formas para acessar as propriedades de um objeto: notação de ponto (.) e notação de colchetes ([]), de forma similar a um array.
+
+Notação de ponto é o que você utiliza quando você sabe o nome da propriedade que você está tentando acessar antecipadamente.
+
+Aqui está um exemplo usando notação de ponto (.) para ler uma propriedade do objeto:
+
+const myObj = {
+  prop1: "val1",
+  prop2: "val2"
+};
+
+const prop1val = myObj.prop1;
+const prop2val = myObj.prop2;
+prop1val teria o valor val1 e prop2val teria o valor val2.
+
+Leia os valores de propriedade de testObj usando a notação de ponto. Defina a variável hatValue igual à propriedade hat do objeto e defina a variável shirtValue igual à propriedade shirt do objeto.*/
+// Configuração
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Altere apenas o código abaixo desta linha
+const hatValue = testObj.hat;      // Altere esta linha
+const shirtValue = testObj.shirt;    // Altere esta linha
+
+/*Acessar propriedades de objeto com notação de colchetes
+A segunda forma para acessar as propriedades de um objeto é a notação de colchetes ([]). Se a propriedade do objeto que você está tentando acessar possui um espaço no seu nome, você precisará usar a notação de colchetes.
+
+No entanto, você ainda pode usar a notação de colchetes nas propriedades dos objetos sem espaços.
+
+Aqui está um exemplo usando a notação de colchetes para ler uma propriedade de um objeto:
+
+const myObj = {
+  "Space Name": "Kirk",
+  "More Space": "Spock",
+  "NoSpace": "USS Enterprise"
+};
+
+myObj["Space Name"];
+myObj['More Space'];
+myObj["NoSpace"];
+myObj["Space Name"] seria a string Kirk, myObj['More Space'] seria a string Spock e myObj["NoSpace"] seria a string USS Enterprise.
+
+Note que os nomes das propriedades com espaços neles precisam estar entre aspas (simples ou duplas).
+
+Leia os valores das propriedades an entree e the drink de testObj usando notação de colchetes e atribua-os a entreeValue e drinkValue respectivamente.*/
+
+// Configuração
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Altere apenas o código abaixo desta linha
+const entreeValue = testObj["an entree"];   // Altere esta linha
+const drinkValue = testObj["the drink"];    // Altere esta linha
