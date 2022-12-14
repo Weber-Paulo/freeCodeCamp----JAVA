d70aa0073a6720623cddda578647e9646893e280
Weber-Paulo <paulomorandini@gmail.com>
2022-12-13 21:11:57 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -1747,3 +1747,13 @@ Faça um objeto que representa um cachorro chamado myDog que contém as propried
 
 Você pode definir essas propriedades do objeto para os valores que deseja, contanto que name seja uma string, legs e tails sejam números, e friends seja um array.*/
 
+const myDog = {
+  // Altere apenas o código abaixo desta linha
+
+"name": "Ring",
+"legs": 4,
+"tails": 1,
+"friends": ["Elden", "Dogs"]
+
+  // Altere apenas o código acima desta linha
+};
