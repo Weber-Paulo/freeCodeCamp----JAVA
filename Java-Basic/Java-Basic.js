2a72cc8bb5dd68083347e1dd6f4e39f2fedffc42
Weber-Paulo <paulomorandini@gmail.com>
2022-12-13 21:24:10 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -1818,3 +1818,34 @@ const testObj = {
 // Altere apenas o código abaixo desta linha
 const entreeValue = testObj["an entree"];   // Altere esta linha
 const drinkValue = testObj["the drink"];    // Altere esta linha
+
+/*Acessar propriedades de objetos com variáveis
+Outro uso de notação de colchetes em objetos é para acessar a propriedade a qual está armazenada como o valor de uma variável. Isso pode ser muito útil para iterar através das propriedades de um objeto ou quando acessando uma tabela de pesquisa.
+
+Aqui está um exemplo de usar uma variável para acessar uma propriedade:
+
+const dogs = {
+  Fido: "Mutt",
+  Hunter: "Doberman",
+  Snoopie: "Beagle"
+};
+
+const myDog = "Hunter";
+const myBreed = dogs[myDog];
+console.log(myBreed);
+A string Doberman seria exibida no console.
+
+Observe que não usamos aspas em torno do nome da variável ao usá-la para acessar a propriedade, porque estamos usando o valor da variável, e não o nome.
+
+Defina a variável playerNumber para ser 16. Então, use a variável para procurar o nome do jogador e atribuí-la a player.*/
+// Configuração
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Altere apenas o código abaixo desta linha
+const playerNumber = 16;  // Altere esta linha
+const player = testObj[playerNumber];   // Altere esta linha
+console.log(player)
