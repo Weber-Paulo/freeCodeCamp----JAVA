19e8604c3093e16d55bb4966d8b8fc8b24ed0f70
Weber-Paulo <paulomorandini@gmail.com>
2022-12-16 22:34:05 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -1907,3 +1907,84 @@ const myDog5 = {
 };
 myDog5.bark = "woof"
 console.log(myDog5)
+
+/*Excluir propriedades de um objeto JavaScript
+Podemos também excluir propriedades de objetos dessa forma:
+
+delete ourDog.bark;
+Exemplo:
+
+const ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+Após a última linha mostrada acima, ourDog se parece com:
+
+{
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+}
+Exclua a propriedade tails de myDog. Você pode usar tanto notação de ponto quanto notação de colchetes.*/
+
+const myDog6 = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog6.tails;
+console.log(myDog6)
+
+/*Usar objetos para pesquisas
+Objetos podem ser pensados como armazenamento de chave/valor, como um dicionário. Se você tem um dado tabular, você pode usar um objeto para pesquisar valores ao invés de uma instrução switch ou uma cadeia de if/else. Isso é mais útil quando você sabe que o seu dado de entrada é limitado para um certo intervalo.
+
+Aqui está um exemplo de um objeto de artigo:
+
+const article = {
+  "title": "How to create objects in JavaScript",
+  "link": "https://www.freecodecamp.org/news/a-complete-guide-to-creating-objects-in-javascript-b0e2450655e8/",
+  "author": "Kaashan Hussain",
+  "language": "JavaScript",
+  "tags": "TECHNOLOGY",
+  "createdAt": "NOVEMBER 28, 2018"
+};
+
+const articleAuthor = article["author"];
+const articleLink = article["link"];
+
+const value = "title";
+const valueLookup = article[value];
+articleAuthor é a string Kaashan Hussain, articleLink é a string https://www.freecodecamp.org/news/a-complete-guide-to-creating-objects-in-javascript-b0e2450655e8/ e valueLookup é a string How to create objects in JavaScript.
+
+Converta a instrução switch em um objeto chamado lookup. Use-o para pesquisar por val e atribua a string associada para a variável result.*/
+
+// Configuração
+function phoneticLookup(val) {
+  let result = "";
+
+  // Altere apenas o código abaixo desta linha
+
+const lookup = {
+"alpha": "Adams",
+"bravo": "Boston",
+"charlie": "Chicago",
+"delta": "Denver",
+"echo": "Easy",
+"foxtrot": "Frank"
+};
+
+result = lookup[val];
+  
+  // Altere apenas o código acima desta linha
+  return result;
+}
+
+phoneticLookup("charlie");
