9d239e07611a30903b5055820f4bc620b047e8cb
Weber-Paulo <paulomorandini@gmail.com>
2022-12-18 18:59:21 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -1988,3 +1988,19 @@ result = lookup[val];
 }
 
 phoneticLookup("charlie");
+
+/*Testar objetos por propriedades
+Às vezes, é útil verificar se a propriedade de um determinado objeto existe ou não. Podemos usar o método de objetos .hasOwnProperty(propname) para determinar se aquele objeto possui o nome de propriedade fornecido. .hasOwnProperty() retorna true ou false se a propriedade for encontrada ou não.
+
+Exemplo
+
+const myObj = {
+  top: "hat",
+  bottom: "pants"
+};
+
+myObj.hasOwnProperty("top");
+myObj.hasOwnProperty("middle");
+O primeiro hasOwnProperty retorna true, enquanto o segundo retorna false.
+
+Modifique a função checkObj para verificar se um objeto passado para a função (obj) contém uma propriedade específica (checkProp). Se a propriedade for encontrada, retorne o valor da propriedade. Se não, retorne "Not Found".*/
