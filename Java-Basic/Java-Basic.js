3fcf42e0a7e44d9f8b1382a2831839a2bb70bb55
Weber-Paulo <paulomorandini@gmail.com>
2022-12-18 19:57:48 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -2004,3 +2004,60 @@ myObj.hasOwnProperty("middle");
 O primeiro hasOwnProperty retorna true, enquanto o segundo retorna false.
 
 Modifique a função checkObj para verificar se um objeto passado para a função (obj) contém uma propriedade específica (checkProp). Se a propriedade for encontrada, retorne o valor da propriedade. Se não, retorne "Not Found".*/
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+      
+  	if (obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp];  
+  } 	else {
+     	return "Not Found";
+  }
+   
+  // Only change code above this line
+}
+
+/*Manipular objetos complexos
+Às vezes, você pode querer armazenar dados em uma Estrutura de Dados flexível. Um objeto JavaScript é uma forma de lidar com dados flexíveis. Eles permitem combinações arbitrárias de strings, numbers, booleans, arrays, functions e objects.
+
+Aqui está um exemplo de estrutura de dados complexas:
+
+const ourMusic = [
+  {
+    "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+    "formats": [ 
+      "CD", 
+      "Cassette", 
+      "LP"
+    ],
+    "gold": true
+  }
+];
+Este é um array que contém um objeto dentro dele. O objeto possui vários pedaços de metadados sobre um álbum. Também possui um array aninhado formats. Se você quiser adicionar mais álbuns, você pode fazer isso adicionando os discos ao array de alto nível. Objetos armazenam dados em uma propriedade, a qual possui um formato de chave-valor. No exemplo acima, "artist": "Daft Punk" é uma propriedade que tem uma chave artist e um valor de Daft Punk.
+
+Observação: você precisará colocar uma vírgula após cada objeto no array, a não ser que ele seja o último objeto no array.
+
+Adicione um novo álbum para o array myMusic. Adicione as strings artist e title, o número release_year e um array de strings formats.*/
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true,
+  },
+  {
+   "artist": "Paulo Weber",
+   "title": "Guitar Man",
+   "release_year": 2022,
+   "formats": [
+     "CD",
+     "LP"
+   ] 
+  }
+];
