5bc4897eadd2a28fdfa3b5762c9f2978c407b4df
Weber-Paulo <paulomorandini@gmail.com>
2023-01-14 16:57:38 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -2440,6 +2440,48 @@ Se namenão corresponder a nenhum dos contatos, então retorne a string No such 
 
 Se propnão corresponder a nenhuma propriedade válida de um contato encontrado para coincidir com nameentão retorne a string No such property.*/
 
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
 
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  const contact = contacts.find(({ firstName }) => firstName === name);
+  if (!contact) {
+    return "No such contact"
+  }
+  if (!contact[prop]) {
+    return "No such property";
+  }
+  return contact[prop];
+}
+
+  // Only change code above this line
+
+lookUpProfile("Akira", "likes");
 
   
