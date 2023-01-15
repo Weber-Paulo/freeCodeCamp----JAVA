281b9311ce2e0574e2c6c3b38b3970815e96830b
Weber-Paulo <paulomorandini@gmail.com>
2023-01-15 12:18:45 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -2637,3 +2637,70 @@ function checkSign(num) {
 }
 
 checkSign(10);
+
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(1 - n);
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5));
+
+/*Usar recursão para criar uma contagem regressiva
+Em um desafio anterior , você aprendeu como usar recursão para substituir laços for. Agora, vamos analisar uma função mais complexa, a qual retorna um array de inteiros consecutivos começando com 1até o número passado para a função.
+
+Como mencionado no desafio anterior, haverá um caso de base . O caso de base diz a função recursiva quando ela não precisa mais chamar a si. É um caso simples onde o valor de retorno já é conhecido. Também haverá uma chamada recursiva , a qual executa a função original com argumentos diferentes. Se a função for escrita corretamente, eventualmente o caso de base será alcançado.
+
+Por exemplo, vamos dizer que você quer escrever uma função recursiva que retorna um array contendo os números de 1até n. Essa função deve aceitar um argumento, n, representando o número final. Então, ela vai precisar chamar a si mesma com valores progressivamente menores, começando em naté chegar a 1. Você poderia escrever a função da seguinte forma:
+
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5));
+O valor [1, 2, 3, 4, 5]será exibido no console.
+
+Inicialmente, isso parece contraintuitivo já que o valor de n diminui , mas os valores no array final estão em ordem crescente . Isso acontece porque a adição no array (push) acontece por último, após uma chamada recursiva ter retornado. No ponto onde né adicionado ao array, countup(n - 1)já foi avaliado e avaliado [1, 2, ..., n -1].
+
+Definimos uma função chamada countdowncom um parâmetro ( n). A função deve usar recursão para retornar um array contendo inteiros naté 1com base no parâmetro n. Se a função é chamada com um número menor que 1, a função deve retornar um array vazio. Por exemplo, chamar essa função com n = 5deve retornar o array [5, 4, 3, 2, 1]. Sua função precisa usar recursão para chamar a si mesma e não depender de nenhum tipo de laço.*/
+
+function countdown(n) {
+  if (n < 1) return [];
+  return [n, ...countdown(n - 1)];
+}
+
+/*Usar recursão para criar um intervalo de números
+Continuando o desafio anterior, fornecemos a você outra oportunidade para criar uma função recursiva para resolver um problema.
+
+Definimos uma função chamada rangeOfNumberscom dois parâmetros. A função deve retornar um array de inteiros a qual começa com um número representado pelo startNumparâmetro e termina com um número representado pelo parâmetro endNum. O número inicial sempre será menor ou igual ao número final. Sua função precisa usar recursão para chamar a si mesma e não deve depender de nenhum tipo de laço. Também deve funcionar para casos onde startNume endNumtiver o mesmo valor.*/
+
+function rangeOfNumbers(startNum, endNum) {
+
+  if(startNum > endNum)
+
+  {
+
+    return [];
+
+  }
+
+  else {
+
+    const rangeArray = rangeOfNumbers(startNum,endNum-1);
+
+    rangeArray.push(endNum);
+
+    return rangeArray;
+
+  }
+}
+
+console.log(rangeOfNumbers(1, 100))
