19f64bbca084653211db0daa2cd9ee0fd49c27fa
Weber-Paulo <paulomorandini@gmail.com>
2023-01-15 11:17:56 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -2525,3 +2525,19 @@ function randomWholeNum() {
 
   return Math.floor(Math.random() * 10);
 }
+
+/*Gerar números inteiros aleatórios dentro de um intervalo
+Ao invés de gerar um número inteiro aleatório entre 0 e um número especificado, como fizemos anteriormente, nós podemos gerar um número inteiro aleatório que fica em um intervalo entre dois números especificados.
+
+Para isso, definiremos um número mínimo mine um número máximo max.
+
+Aqui está a fórmula que usaremos. Leve um momento para ler e entender o que esse código está fazendo:
+
+Math.floor(Math.random() * (max - min + 1)) + min
+Crie uma função chamada randomRangeque recebe um intervalo de myMine myMaxe retorne um número inteiro aleatório que é maior ou igual a myMin, e é menor ou igual a myMax.*/
+
+function randomRange(myMin, myMax) {
+  // Altere apenas o código abaixo desta linha
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  // Altere apenas o código acima desta linha
+}
