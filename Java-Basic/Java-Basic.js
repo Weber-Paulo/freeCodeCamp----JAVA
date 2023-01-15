17032dc054abc2ccc3b0195e46714ed0b633b4fe
Weber-Paulo <paulomorandini@gmail.com>
2023-01-15 11:55:16 -0300

    Basic

--- Java-Basic/Java-Basic.js
+++ Java-Basic/Java-Basic.js
@@ -2599,3 +2599,41 @@ function checkEqual(a, b) {
   }
   
   checkEqual(1, 2);
+
+  /*Usar operadores de condições múltiplas (ternários)
+No desafio anterior, você usou um único operador condicional. Você também pode encadear os dois juntos para verificar por condições múltiplas.
+
+A seguinte função usa as instruções if, else ife elsepara verificar múltiplas condições:
+
+function findGreaterOrEqual(a, b) {
+  if (a === b) {
+    return "a and b are equal";
+  }
+  else if (a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater";
+  }
+}
+A função acima pode ser rescrita usando operadores de múltiplas condições (operador ternário):
+
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" 
+    : (a > b) ? "a is greater" 
+    : "b is greater";
+}
+É considerada a melhor prática para formatar operadores de múltiplas condições, já que cada condição está em uma linha separada, como mostrado acima. Usar operadores de condições múltiplas sem a indentação adequada pode dificultar a leitura do seu código. Por exemplo:
+
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+}
+Na função checkSign, use operadores de condições múltiplas - seguindo o formato recomendado usado em findGreaterOrEqual- para verificar se um número é positivo, negativo ou zero. A função deve retornar positive, negativeou zero.*/
+
+function checkSign(num) {
+  return (num === 0) ? "zero" 
+    : (num > 0) ? "positive" 
+    : "negative";
+}
+
+checkSign(10);
